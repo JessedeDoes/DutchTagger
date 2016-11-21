a2e269dbc4d9573887415f8d785a6cdfc642e4c4
jessededoes <jesse.dedoes@inl.nl>
2016-11-21 14:43:13 +0100

    .

--- scripts/testDutchTagger.sh
+++ scripts/testDutchTagger.sh
@@ -1,4 +1,8 @@
 JAR=./target/DutchTagger-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+
 CORPUSDIR=/vol1/Corpus
 LEXICON=/vol1/Corpus/DutchTagger/Data/spelling.tab
-java -Xmx60g -cp $JAR nl.namescape.tagging.ImpactTaggerLemmatizerClient --useExternalLexicon true --word2vecFile=$CORPUSDIR/PlainTextDumps/sonar.vectors.bin --tokenize=true $CORPUSDIR/Tagger/Last/withMoreVectorrs $LEXICON "$1" "$2"
+VECTORS=$CORPUSDIR/DutchTagger/Data/sonar.vectors.bin
+MODEL=$CORPUSDIR/DutchTagger/Data/withMoreVectorrs
+
+java -Xmx60g -cp $JAR nl.namescape.tagging.ImpactTaggerLemmatizerClient --useExternalLexicon true --word2vecFile=$VECTORS --tokenize=true $MODEL $LEXICON "$1" "$2"
